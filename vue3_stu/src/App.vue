fb5d3c21000b1a14016c6b96a1c6ae99f4a5a88d
timmy338 <66931238+timmy338@users.noreply.github.com>
2022-07-24 15:59:09 +0800

    master

---
+++ vue3_stu/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+
+    <iframe
+      src="//player.bilibili.com/player.html?aid=804122638&bvid=BV1Zy4y1K7SH&cid=377285629&page=155"
+      scrolling="no"
+      border="0"
+      frameborder="no"
+      framespacing="0"
+      allowfullscreen="true"
+      width="1000"
+      height="800"
+    >
+    </iframe>
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
