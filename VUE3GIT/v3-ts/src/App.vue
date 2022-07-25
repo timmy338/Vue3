70db3214435fade4386c2f7075547977e988d788
timmy338 <66931238+timmy338@users.noreply.github.com>
2022-07-25 17:43:45 +0800

    引入elementPLUS組件庫

--- VUE3GIT/v3-ts/src/App.vue
+++ VUE3GIT/v3-ts/src/App.vue
@@ -1,9 +1,13 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div>
+    <el-button type="primary">Primary</el-button>
+
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view />
   </div>
-  <router-view/>
 </template>
 
 <style>
